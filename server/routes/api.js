1d078f24f0b4d3143664f665e1dff17ee155a1a3
Mohamed ZITOUNI <zitouni.mohamed.email@gmail.com>
2017-11-21 10:23:12 +0100

    finish the loading of stocks by http request

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,16 +12,32 @@ router.get('/', (req, res) => {
 });
 
 // Get all positions
-router.get('/posts', (req, res) => {
+router.get('/stocks', (req, res) => {
   // Get posts from the mock API
   // This should ideally be replaced with a service that connects to MongoDB
-  axios.get(`${API}/posts`)
-    .then(posts => {
-      res.status(200).json(posts.data);
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    });
+  // axios.get(`${API}/posts`)
+  //   .then(posts => {
+  //     res.status(200).json(posts.data);
+  //   })
+  //   .catch(error => {
+  //     res.status(500).send(error)
+  //   });
+  res.json(
+    [
+      {
+        "ref": "Stock1",
+        "quantite": 250
+      },
+      {
+        "ref": "Stock2",
+        "quantite": 150
+      },
+      {
+        "ref": "Stock3",
+        "quantite": 284
+      }
+    ]
+  );
 });
 
 module.exports = router;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {HttpModule} from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { StockComponent } from './stock/stock.component';
+import {StockService} from './stock/stock.service';
 
-
+const routes = [
+  {
+    path: '',
+    redirectTo: 'stocks',
+    pathMatch: 'full'
+  },
+  {
+    path: 'stocks',
+    component: StockComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [StockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
